355cd001504813dab189e2785de6c3bd44f01e67
rdwanmuhamad <mrdwn624@gmail.com>
2021-10-23 11:45:21 +0800

    pengenalan views dan menambahkan file views

--- belajar_django/urls.py
+++ belajar_django/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello World!")
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
+    path('', views.index),
+    path('about/', views.about),
 ]
