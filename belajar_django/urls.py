9e74df8a3ed95b95e65c70e81d396d082a0ea191
rdwanmuhamad <mrdwn624@gmail.com>
2021-10-27 06:08:16 +0800

    menggunakan urls pada app

--- belajar_django/urls.py
+++ belajar_django/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('blog/', include('blog.urls')),
-    path('about/', views.about),
+    path('about/',include('about.urls')),
 ]
