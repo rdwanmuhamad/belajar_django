16c73ba5d92a4f2b2b6bdbb9a7357752d785c977
rdwanmuhamad <mrdwn624@gmail.com>
2021-11-11 10:39:15 +0800

    model field, tipe data, options dan migrations

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Post (models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    email = models.EmailField(default='nama@email.test')
+    alamat = models.CharField(max_length=255, blank=True,)
+    
 
     def __str__(self):
         return "{}".format(self.title)
